4699362faa10e851d5549d1631d5c98ecc11b711
Lipegithub7 <flpgstsilva@gmail.com>
2024-11-05 17:09:48 -0300

    lipe

---
+++ lib/pages/configuracao.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Configuracao extends StatefulWidget {
+  const Configuracao({super.key});
+
+  @override
+  State<Configuracao> createState() => _ConfiguracaoState();
+}
+
+class _ConfiguracaoState extends State<Configuracao> {
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(
+      width: 50,
+      height: 50,
+    );
+  }
+}
