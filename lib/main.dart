5f7b6dbd6bd4df3bc5b5846baaaf89cca63fa528
Draak18 <darkmerman07@gmail.com>
2024-11-22 09:29:28 -0300

    botaoSair

--- lib/main.dart
+++ lib/main.dart
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
           primarySwatch: Colors.purple,
           visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
-        initialRoute: '/resultado',
+        initialRoute: '/',
         routes: {
           '/': (context) => const HomePage(),
           '/cadastro': (context) => const Cadastro(),
