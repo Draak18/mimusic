376a93b726a6f828aa955a9682158a1c81dc27ef
Gabizete <gabrielledelimaq@gmail.com>
2024-12-05 16:15:59 -0300

    CHUPAFUNCIONOU

--- lib/pages/telamusica.dart
+++ lib/pages/telamusica.dart
@@ -19,18 +19,26 @@ class _TelaMusicaState extends State<TelaMusica> {
     super.initState();
 
     // Inicializar o controlador do YouTube
+    String? videoId = YoutubePlayer.convertUrlToId(widget.musica['audio']);
+    if (videoId == null) {
+      debugPrint('URL do vídeo inválida: ${widget.musica['audio']}');
+      return;
+    }
+
     _youtubeController = YoutubePlayerController(
-      initialVideoId: YoutubePlayer.convertUrlToId(widget.musica['audio'])!,
+      initialVideoId: videoId,
       flags: const YoutubePlayerFlags(
-        autoPlay: false, // Não reproduz automaticamente
-        mute: false,
+        autoPlay: true, // Vídeo começa automaticamente
+        mute: false, // Áudio ativado
+        hideControls: true, // Controles ocultos
+        showLiveFullscreenButton: false, // Remover o botão de tela cheia
       ),
     );
   }
 
   @override
   void dispose() {
-    _youtubeController.dispose(); // Liberar o controlador ao sair da tela
+    _youtubeController.dispose();
     super.dispose();
   }
 
@@ -40,119 +48,161 @@ class _TelaMusicaState extends State<TelaMusica> {
     } else {
       _youtubeController.play();
     }
-    setState(() {}); // Atualiza o estado para refletir a mudança
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.black,
+        backgroundColor: const Color.fromRGBO(17, 17, 17, 1),
         leading: IconButton(
           icon: const Icon(Icons.arrow_back, color: Colors.white),
           onPressed: () => Navigator.pop(context),
         ),
+        title: Center(
+          // Centralizando o título
+          child: RichText(
+            text: TextSpan(
+              children: [
+                TextSpan(
+                  text: "MI",
+                  style: GoogleFonts.montserrat(
+                    color: Colors.white,
+                    fontSize: 24,
+                    fontWeight: FontWeight.w800,
+                  ),
+                ),
+                TextSpan(
+                  text: "MUSIC",
+                  style: GoogleFonts.montserrat(
+                    color: const Color.fromRGBO(189, 0, 243, 1),
+                    fontSize: 24,
+                    fontWeight: FontWeight.w800,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
       body: Container(
-        color: Colors.black,
-        child: Column(
-          children: [
-            // Título e artista
-            Container(
-              padding: const EdgeInsets.all(16),
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [Colors.purple.shade800, Colors.purple.shade400],
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                ),
-                borderRadius: const BorderRadius.only(
-                  bottomLeft: Radius.circular(20),
-                  bottomRight: Radius.circular(20),
-                ),
-              ),
-              child: Column(
-                children: [
-                  Text(
-                    widget.musica['title'],
-                    style: GoogleFonts.poppins(
-                      color: Colors.white,
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
+        color: const Color.fromRGBO(17, 17, 17, 1),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              children: [
+                // Título e artista
+                Container(
+                  padding: const EdgeInsets.all(16),
+                  decoration: BoxDecoration(
+                    gradient: const LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment(0.0, 0.9),
+                      colors: [Color(0xFF560A6C), Colors.black],
                     ),
+                    borderRadius: BorderRadius.circular(10),
                   ),
-                  Text(
-                    widget.musica['artista'],
-                    style: GoogleFonts.poppins(
-                      color: Colors.white70,
-                      fontSize: 18,
-                    ),
+                  child: Column(
+                    children: [
+                      Text(
+                        widget.musica['title'],
+                        style: GoogleFonts.poppins(
+                          color: Colors.white,
+                          fontSize: 28,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        widget.musica['artista'],
+                        style: GoogleFonts.poppins(
+                          color: Colors.white70,
+                          fontSize: 20,
+                        ),
+                      ),
+                      const SizedBox(height: 16),
+                      // Imagem
+                      Image.network(
+                        widget.musica['imagem'],
+                        width: 300,
+                        height: 300,
+                        fit: BoxFit.cover,
+                      ),
+                    ],
                   ),
-                ],
-              ),
-            ),
-            const SizedBox(height: 16),
-            
-            Image.network(
-              widget.musica['imagem'], // Substitua pelo URL ou caminho da imagem
-              width: 300, // Ajuste conforme necessário
-              height: 300, // Ajuste conforme necessário
-              fit:BoxFit.cover, // Define como a imagem se ajustará ao container
-            ),
-            const SizedBox(height: 16),
-            Center(
-              child: AnimatedSwitcher(
-                duration: const Duration(milliseconds: 300),
-                child: IconButton(
-                  key: ValueKey<bool>(_youtubeController.value.isPlaying),
-                  icon: Icon(
-                    _youtubeController.value.isPlaying
-                        ? Icons.pause
-                        : Icons.play_arrow,
-                    color: Colors.white,
-                    size: 36,
-                  ),
-                  onPressed: _togglePlayPause,
                 ),
-              ),
-            ),
-            const SizedBox(height: 16),
+                const SizedBox(height: 16),
 
-            // Player de vídeo do YouTube (invisível)
-            SizedBox(
-              height: 0, // Altura zero para ocultar o player
-              child: YoutubePlayer(
-                controller: _youtubeController,
-                showVideoProgressIndicator:
-                    false, // Não mostrar o indicador de progresso
-                progressIndicatorColor: Colors.purple,
-                aspectRatio: 16 / 9, // Usar um aspecto válido
-              ),
-            ),
-            const SizedBox(height: 16),
-
-            // Texto da música
-            Expanded(
-              child: Container(
-                padding: const EdgeInsets.all(16),
-                margin: const EdgeInsets.symmetric(horizontal: 16),
-                decoration: BoxDecoration(
-                  color: Colors.purple.shade800,
-                  borderRadius: BorderRadius.circular(16),
-                ),
-                child: SingleChildScrollView(
-                  child: Text(
-                    widget.musica['letra'] ?? "Letra não disponível.",
-                    style: GoogleFonts.poppins(
-                      fontSize: 16,
-                      color: Colors.white,
-                    ),
-                    textAlign: TextAlign.center,
+                // Player oculto (não exibe o player na tela)
+                SizedBox(
+                  height: 0, // Player escondido
+                  child: YoutubePlayer(
+                    controller: _youtubeController,
+                    showVideoProgressIndicator: false, // Não exibe o progresso
+                    progressIndicatorColor: Colors.purple,
+                    aspectRatio:
+                        16 / 9, // Não é relevante, mas mantém a consistência
                   ),
                 ),
-              ),
+
+                // Controles de reprodução
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    IconButton(
+                      icon: const Icon(Icons.skip_previous),
+                      color: Colors.white,
+                      iconSize: 30,
+                      onPressed: () {
+                        // Implementar lógica para música anterior
+                      },
+                    ),
+                    IconButton(
+                      icon: Icon(
+                        _youtubeController.value.isPlaying
+                            ? Icons.play_arrow // Ícone de pause
+                            : Icons.pause, // Ícone de play
+                        color: Colors.white,
+                        size: 42,
+                      ),
+                      onPressed: _togglePlayPause,
+                    ),
+                    IconButton(
+                      icon: const Icon(Icons.skip_next),
+                      color: Colors.white,
+                      iconSize: 30,
+                      onPressed: () {
+                        // Implementar lógica para próxima música
+                      },
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 16),
+
+                // Texto da música
+                Container(
+                  padding: const EdgeInsets.all(16),
+                  margin: const EdgeInsets.symmetric(horizontal: 16),
+                  decoration: BoxDecoration(
+                    color: const Color(0xFF3E2941),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: SingleChildScrollView(
+                    child: Text(
+                      widget.musica['letra'] ?? "Letra não disponível.",
+                      style: GoogleFonts.poppins(
+                        fontSize: 16,
+                        color: Colors.white,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 16),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
